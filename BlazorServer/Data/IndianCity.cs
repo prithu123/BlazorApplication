3fdf7523c6dda1cecd6b8c2bf43efcb23a13d404
prithu123 <prithu.paul1907@gmail.com>
2020-05-18 16:52:28 +0530

    Devexpress cosmos integration

---
+++ BlazorServer/Data/IndianCity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Data
+{
+    public class IndianCity :City
+    {
+        public String state { get; set; }
+        public String region { get; set; }
+    }
+}
---
+++ BlazorServer/Data/Legend.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureCosmos.Models
+{
+    public class Legend
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "legendname")]
+        public string legendname { get; set; }
+
+        [JsonProperty(PropertyName = "invention")]
+        public string invention { get; set; }
+    }
+}
